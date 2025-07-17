61ab5b7d1a7fcd80bd52de67355afe895611ad81
GuliGuli228 <128092251+GuliGuli228@users.noreply.github.com>
2025-07-17 22:34:28 +0300

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,31 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        /*--------ОБЪЕКТЫ---------*/
+        Linked_list linked_list = new Linked_list();
+        DoublyLinkedList doublyLinkedList = new DoublyLinkedList();
+
+        /*--------МЕТОДЫ---------*/
+//        linked_list.insert(10);
+//        linked_list.insert(20);
+//        linked_list.insert(30);
+//        linked_list.insert(40);
+//        linked_list.insert(50);
+//        linked_list.insert(60);
+//        linked_list.insert(31,3);
+//        linked_list.display();
+//        System.out.println(linked_list.length());
+        /*----------------------------------------------*/
+        doublyLinkedList.insert(10);
+        doublyLinkedList.insert(20);
+        doublyLinkedList.insert(30);
+        doublyLinkedList.insert(40);
+        doublyLinkedList.insert(29,3);
+        doublyLinkedList.display();
+
+
+    }
+}
