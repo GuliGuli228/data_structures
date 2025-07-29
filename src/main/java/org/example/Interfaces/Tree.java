58424c98105963efb677e9576997b655b922a96d
GuliGuli228 <128092251+GuliGuli228@users.noreply.github.com>
2025-07-30 02:49:18 +0300

    changed return type for show method

--- src/main/java/org/example/Interfaces/Tree.java
+++ src/main/java/org/example/Interfaces/Tree.java
@@ -3,7 +3,7 @@ package org.example.Interfaces;
 public interface Tree<T, N> {
     void add (T value);
     void deleteByValue(T value);
-    void show();
+    String show();
     void update(T from, T to);
     N BFS(T value);
     N DPS(T value);
