bb8a581fb714de88d8ce9619ff4404ef9c842fa5
GuliGuli228 <128092251+GuliGuli228@users.noreply.github.com>
2025-07-31 02:34:29 +0300

    added test pattern for AVL Tree

--- src/test/java/org/example/ComplexStructures/AVLTreeTest.java
+++ src/test/java/org/example/ComplexStructures/AVLTreeTest.java
@@ -1,4 +1,11 @@
+package org.example.ComplexStructures;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class AVLTreeTest {
-  
+    AVLTree<Integer> tree;
+
 }
