17548b9c8f69c8dd1e567ff788946c7a90721803
GuliGuli228 <128092251+GuliGuli228@users.noreply.github.com>
2025-08-05 01:12:30 +0300

    refactored methods

--- src/main/java/org/example/BasicStructures/DoublyLinkedList.java
+++ src/main/java/org/example/BasicStructures/DoublyLinkedList.java
@@ -1,6 +1,9 @@
 package org.example.BasicStructures;
 
+import org.apache.commons.lang.NullArgumentException;
 import org.example.AbstracClasses.AbstractList;
+import org.example.Exceptions.EmptyDoublyLinkedListException;
+import org.example.Exceptions.EmptyLinkedListException;
 import org.example.Interfaces.List;
 
 import java.util.NoSuchElementException;
@@ -17,7 +20,7 @@ public class DoublyLinkedList<T> extends AbstractList implements List<T> {
             super(value);
             this.next = next;
         }
-        protected DoublyLinkedNode(T value, DoublyLinkedNode next, DoublyLinkedNode prev){
+        protected DoublyLinkedNode(T value, DoublyLinkedNode prev, DoublyLinkedNode next){
             super(value);
             this.next = next;
             this.prev = prev;
@@ -34,59 +37,103 @@ public class DoublyLinkedList<T> extends AbstractList implements List<T> {
 
     /*-----Variables-----*/
     DoublyLinkedNode head;
-    int lenght = 0;
+    DoublyLinkedNode tail;
+    int length = 0;
     /*------------------*/
 
     @Override
     public void insertAt(T value) {
-        if(this.isEmpty())  head = new DoublyLinkedNode(value, null,null);
+        if(value == null) throw new NullArgumentException("value");
+        if(this.isEmpty()) {
+            head = new DoublyLinkedNode(value);
+            tail = head;
+        }
         else{
             DoublyLinkedNode current = head;
             while(current.next != null){
                 current = current.next;
             }
             current.next = new DoublyLinkedNode(value, current, null);
+            tail = current.next;
         }
-        lenght++;
+        length++;
     }
+
     public void insertAt(T value, int place){
-        if (this.isEmpty() || lenght-1 > place) {
-            this.insertAt(value);
-            return;
+        if(value == null) throw new NullArgumentException("value");
+        if(place < 0 || place > length) throw new IndexOutOfBoundsException("place is out of bounds");
+        if(this.isEmpty()) throw new EmptyDoublyLinkedListException();
+
+        DoublyLinkedNode node = new DoublyLinkedNode(value);
+        if(place == 0){
+            head.prev = node;
+            node.next = head;
+            head = node;
         }
-        else{
+        if(place == length){
+            tail.next = node;
+            node.prev = tail;
+            tail = node;
+        }
+        else {
             DoublyLinkedNode current = head;
-            for(int i = 0; i < place; i++){
+            for (int i =0; i < place - 1; i++){
                 current = current.next;
             }
-
+            DoublyLinkedNode tempNode =  current.next;
+            current.next = node;
+            node.prev = current;
+            node.next =  tempNode;
+            tempNode.prev = node;
         }
+        length++;
     }
 
     @Override
     public void removeAt(int place) {
-        if(this.isEmpty()) System.out.println("List is empty");
-        else{
-            int counter = 1;
-            Node current = head;
+        if (place < 0 || place+1 > length) throw new IndexOutOfBoundsException("place is out of bounds");
+        if (this.isEmpty()) throw new EmptyDoublyLinkedListException();
+
+        if(place == 0){
+            DoublyLinkedNode current = head.next;
+            head.next = null;
+            current.prev = null;
+            head = current;
         }
+        if(place == length-1){
+            DoublyLinkedNode current = tail.prev;
+            tail.prev = null;
+            current.next = null;
+            tail = current;
+        }
+        else{
+            DoublyLinkedNode current = head;
+            for (int i =0; i < place - 1; i++){
+                current = current.next;
+            }
+            DoublyLinkedNode tempNode =  current.next;
+            tempNode.prev = null;
+            current.next = tempNode.next;
+            tempNode.next = null;
+            current.next.prev = current;
+        }
+        length--;
     }
 
     @Override
     public String show() {
-        if(this.isEmpty()){
-            System.out.println("List is empty");
-            throw new NoSuchElementException();
-        }
-        else{
-            StringBuilder str = new StringBuilder("[ ");
+        if (this.isEmpty())throw new EmptyDoublyLinkedListException();
+        else {
             DoublyLinkedNode current = head;
-            while (current!= null){
-                str.append(current.getValue()).append("->");
+            StringBuilder output = new StringBuilder("List: [ ");
+
+            while (current != null) {
+                output.append(current.getValue());
+                if (current.next != null) output.append(" , ");
                 current = current.next;
             }
-            str.append("]");
-            return str.toString();
+            output.append(" ]");
+            return output.toString();
         }
     }
 
@@ -97,14 +144,18 @@ public class DoublyLinkedList<T> extends AbstractList implements List<T> {
 
     @Override
     public void update(T value, int place) {
-        try {
+        if (place < 0 || place + 1 > length) throw new IndexOutOfBoundsException("place is out of bounds");
+        if (value == null) throw new NullArgumentException("value");
+        if (this.isEmpty()) throw new EmptyDoublyLinkedListException();
+
+        if (place == 0) head.setValue(value);
+        if (place == length-1) tail.setValue(value);
+        else{
             DoublyLinkedNode current = head;
-            for (int i = 0; i < place; i++){
+            for (int i =0; i < place; i++){
                 current = current.next;
             }
             current.setValue(value);
-        } catch (NullPointerException e) {
-            System.out.println("No such element to update +" + e.getMessage());;
         }
     }
 }
---
+++ src/main/java/org/example/ComplexStructures/Heap.java
@@ -0,0 +1,4 @@
+package org.example.ComplexStructures;
+
+public class Heap {
+}
