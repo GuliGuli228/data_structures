2504ccf6e678d2a04dbfa019eb426ae36ebe2066
GuliGuli228 <128092251+GuliGuli228@users.noreply.github.com>
2025-08-05 23:53:05 +0300

    started working on Circular list

---
+++ src/main/java/org/example/ComplexStructures/CircularList.java
@@ -0,0 +1,14 @@
+package org.example.ComplexStructures;
+
+import org.example.BasicStructures.DoublyLinkedList;
+
+
+public class CircularList<T> extends DoublyLinkedList<T> {
+
+    /*---Variables----*/
+    DoublyLinkedNode head;
+    DoublyLinkedNode tail;
+    /*----------------*/
+
+
+}
---
+++ src/main/java/org/example/Exceptions/EmptyBinaryTreeException.java
@@ -0,0 +1,7 @@
+package org.example.Exceptions;
+
+public class EmptyBinaryTreeExeption extends RuntimeException {
+  public EmptyBinaryTreeExeption(String message) {
+    super(message);
+  }
+}
