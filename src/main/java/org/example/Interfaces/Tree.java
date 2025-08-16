236acedb662c39429cad980dd4b1151e7d2e3e7a
GuliGuli228 <128092251+GuliGuli228@users.noreply.github.com>
2025-08-17 02:31:18 +0300

    refactored show

--- src/main/java/org/example/Interfaces/Tree.java
+++ src/main/java/org/example/Interfaces/Tree.java
@@ -16,7 +16,7 @@ package org.example.Interfaces;
 public interface Tree<T, N> {
     void add (T value);
     void deleteByValue(T value);
-    String show();
+    void show();
     void update(T from, T to);
     N BFS(T value);
     N DPS(T value);
