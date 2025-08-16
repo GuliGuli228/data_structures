6dd4031118449b20068c4f817e74237568db3dfa
GuliGuli228 <128092251+GuliGuli228@users.noreply.github.com>
2025-08-16 23:49:43 +0300

    fixed bugs

--- src/test/java/org/example/ComplexStructures/RedBlackTreeTest.java
+++ src/test/java/org/example/ComplexStructures/RedBlackTreeTest.java
@@ -20,6 +20,70 @@ public class RedBlackTreeTest {
 
     private RedBlackTree<Integer> tree;
 
+    private boolean checkRBTProp(RedBlackTree<Integer> tree) throws Exception{
+        RedBlackTree.RBTNode root = getRoot(tree);
+
+        if (getRBTColors(root) != RBTColors.BLACK) return false;
+        if (checkBlackHeight(root) == 0 ) return false;
+        if (!checkRedNodesProp(root)) return false;
+        return true;
+    }
+    private RedBlackTree.RBTNode getRoot (RedBlackTree<Integer> tree) throws Exception{
+        Field rootField = BinarySearchTree.class.getDeclaredField("root");
+        rootField.setAccessible(true);
+        return (RedBlackTree.RBTNode) rootField.get(tree);
+    }
+
+    private RedBlackTree.RBTNode getLeft (RedBlackTree.RBTNode node) throws Exception{
+        Method getLeftMethod = RedBlackTree.RBTNode.class.getDeclaredMethod("getLeft");
+        getLeftMethod.setAccessible(true);
+        return (RedBlackTree.RBTNode) getLeftMethod.invoke(node);
+    }
+
+    private RedBlackTree.RBTNode getRight (RedBlackTree.RBTNode node) throws Exception{
+        Method getRightMethod = RedBlackTree.RBTNode.class.getDeclaredMethod("getRight");
+        getRightMethod.setAccessible(true);
+        return (RedBlackTree.RBTNode) getRightMethod.invoke(node);
+    }
+
+    private RBTColors getRBTColors(RedBlackTree<Integer>.RBTNode node) throws Exception{
+        Method getColorMethod = RedBlackTree.RBTNode.class.getDeclaredMethod("getColor");
+        getColorMethod.setAccessible(true);
+        return (RBTColors) getColorMethod.invoke(node);
+    }
+
+    private RedBlackTree.RBTNode getParent(RedBlackTree<Integer>.RBTNode node) throws Exception{
+        Method getParentMethod = RedBlackTree.RBTNode.class.getDeclaredMethod("getParent");
+        getParentMethod.setAccessible(true);
+        return (RedBlackTree.RBTNode) getParentMethod.invoke(node);
+    }
+    private int checkBlackHeight(RedBlackTree<Integer>.RBTNode node) throws Exception{
+
+        if (node instanceof RedBlackTree.NILNode) return 1;
+        int LeftBH = checkBlackHeight(getLeft(node));
+        if (LeftBH==0) return 0;
+        int RightBH = checkBlackHeight(getRight(node));
+        if (RightBH==0) return 0;
+
+        if (LeftBH != RightBH) return 0;
+        if (getRBTColors(node) == RBTColors.BLACK) return LeftBH +1;
+        else return LeftBH;
+    }
+    private boolean checkRedNodesProp(RedBlackTree.RBTNode node) throws Exception {
+        Stack <RedBlackTree.RBTNode> stack = new Stack<>();
+        stack.push(node);
+
+        while (!stack.isEmpty()){
+            RedBlackTree.RBTNode pop = stack.pop();
+            if (getParent(pop) != null){
+                if (getRBTColors(pop) == RBTColors.RED && getRBTColors(getParent(pop)) ==  RBTColors.RED) return false;
+            }
+            if (!(getRight(pop) instanceof RedBlackTree.NILNode)) stack.push(getRight(pop));
+            if(!(getLeft(pop) instanceof RedBlackTree.NILNode)) stack.push(getLeft(pop));
+        }
+        return true;
+    }
+
     @BeforeEach
     void setUp() {
         tree = new RedBlackTree<>();
@@ -120,71 +184,5 @@ public class RedBlackTreeTest {
         tree.add(20);
         tree.add(30);
         assertNull(tree.DPS(25));
-    }
-
-
-
-    private boolean checkRBTProp(RedBlackTree<Integer> tree) throws Exception{
-        RedBlackTree.RBTNode root = getRoot(tree);
-
-        if (getRBTColors(root) != RBTColors.BLACK) return false;
-        if (checkBlackHeight(root) == 0 ) return false;
-        if (!checkRedNodesProp(root)) return false;
-        return true;
-    }
-    private RedBlackTree.RBTNode getRoot (RedBlackTree<Integer> tree) throws Exception{
-        Field rootField = BinarySearchTree.class.getDeclaredField("root");
-        rootField.setAccessible(true);
-        return (RedBlackTree.RBTNode) rootField.get(tree);
-    }
-
-    private RedBlackTree.RBTNode getLeft (RedBlackTree.RBTNode node) throws Exception{
-        Method getLeftMethod = RedBlackTree.RBTNode.class.getDeclaredMethod("getLeft");
-        getLeftMethod.setAccessible(true);
-        return (RedBlackTree.RBTNode) getLeftMethod.invoke(node);
-    }
-
-    private RedBlackTree.RBTNode getRight (RedBlackTree.RBTNode node) throws Exception{
-        Method getRightMethod = RedBlackTree.RBTNode.class.getDeclaredMethod("getRight");
-        getRightMethod.setAccessible(true);
-        return (RedBlackTree.RBTNode) getRightMethod.invoke(node);
-    }
-
-    private RBTColors getRBTColors(RedBlackTree<Integer>.RBTNode node) throws Exception{
-        Method getColorMethod = RedBlackTree.RBTNode.class.getDeclaredMethod("getColor");
-        getColorMethod.setAccessible(true);
-        return (RBTColors) getColorMethod.invoke(node);
-    }
-
-    private RedBlackTree.RBTNode getParent(RedBlackTree<Integer>.RBTNode node) throws Exception{
-        Method getParentMethod = RedBlackTree.RBTNode.class.getDeclaredMethod("getParent");
-        getParentMethod.setAccessible(true);
-        return (RedBlackTree.RBTNode) getParentMethod.invoke(node);
-    }
-    private int checkBlackHeight(RedBlackTree<Integer>.RBTNode node) throws Exception{
-
-        if (node instanceof RedBlackTree.NILNode) return 1;
-        int LeftBH = checkBlackHeight(getLeft(node));
-        if (LeftBH==0) return 0;
-        int RightBH = checkBlackHeight(getRight(node));
-        if (RightBH==0) return 0;
-
-        if (LeftBH != RightBH) return 0;
-        if (getRBTColors(node) == RBTColors.BLACK) return LeftBH +1;
-        else return LeftBH;
-    }
-    private boolean checkRedNodesProp(RedBlackTree.RBTNode node) throws Exception {
-        Stack <RedBlackTree.RBTNode> stack = new Stack<>();
-        stack.push(node);
-
-        while (!stack.isEmpty()){
-            RedBlackTree.RBTNode pop = stack.pop();
-            if (getParent(pop) != null){
-                if (getRBTColors(pop) == RBTColors.RED && getRBTColors(getParent(pop)) ==  RBTColors.RED) return false;
-            }
-            if (!(getRight(pop) instanceof RedBlackTree.NILNode)) stack.push(getRight(pop));
-            if(!(getLeft(pop) instanceof RedBlackTree.NILNode)) stack.push(getLeft(pop));
-        }
-        return true;
     }
 }
