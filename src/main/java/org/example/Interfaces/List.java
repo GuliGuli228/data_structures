405beae7c4912767953eecaf5cc34233b02c6141
GuliGuli228 <128092251+GuliGuli228@users.noreply.github.com>
2025-08-16 23:45:17 +0300

    Changed getSize to size

--- src/main/java/org/example/Interfaces/List.java
+++ src/main/java/org/example/Interfaces/List.java
@@ -12,9 +12,11 @@ package org.example.Interfaces;
  */
 
 public interface List<T> {
-     void insertAt(T value);
+     void add(T value);
      void removeAt(int place);
      String show();
      boolean isEmpty();
      void update(T value, int place);
+     int size();
+     Object[] toArray();
 }
